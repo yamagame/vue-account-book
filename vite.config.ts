3048b3310daa2f258a5d24621d1a6a6ca36d3c1e
yamagame <yamagame@mac.com>
2023-06-28 08:34:01 +0900

    docsディレクトリ作成

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: './docs',
+  },
 })
