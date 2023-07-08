76f5754a63f07b2d665e9df71ad4b55563d34515
yamagame <yamagame@mac.com>
2023-07-08 18:12:46 +0900

    emits実装

--- src/components/CopyButton.vue
+++ src/components/CopyButton.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+interface Emits {
+  (e: 'copy', // 関数名
+    v: null // 引数の型
+  ): void; // 返り値の型
+}
 const props = defineProps<{
   text: string
 }>()
+const emits = defineEmits<Emits>();
 const changeMessage = function () {
+  emits('copy', null);
   if (navigator.clipboard) {
     navigator.clipboard.writeText(props.text).then(() => {
       alert("コピーしました")
